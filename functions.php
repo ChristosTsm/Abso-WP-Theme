357750250dec6a301af4370159b0133e0c058eaa
ChristosTsm <rayden.tsm@gmail.com>
2020-11-05 10:31:08 +0200

    path variables

--- functions.php
+++ functions.php
@@ -10,6 +10,10 @@ if( ! defined( 'ABSO_DIR_PATH' ) ) {
     define( 'ABSO_DIR_PATH', untrailingslashit( get_template_directory() ) );
 }
 
+if( ! defined( 'ABSO_DIR_URI' ) ) {
+    define( 'ABSO_DIR_URI', untrailingslashit( get_template_directory_uri() ) );
+}
+
 require_once ABSO_DIR_PATH . '/inc/helpers/autoloader.php';
 
 function abso_get_theme_instance() {
@@ -22,24 +26,10 @@ abso_get_theme_instance();
 // Enqueue styles and scripts
 function abso_enqueue_scripts( ) {
 
-    // Register CSS Here
-    wp_register_style( 'custom-styles', get_stylesheet_uri(  ), array( ), filemtime( get_template_directory( ) . '/style.css' ), 'all'  );
-    wp_register_style( 'bootstrap-css', get_template_directory_uri( ) . '/assets/src/library/css/bootstrap.min.css',  false,  'all' );
+
     
 
-    // Register Scripts Here
-    wp_register_script( 'main-js',  get_template_directory_uri(  ) . '/assets/js/main.js', array(), '1.0.0', true );
-    wp_register_script( 'bootstrap-js',  get_template_directory_uri( ) . '/assets/src/library/js/bootstrap.min.js', array( 'jquery' ), false, true );
 
-
-    // Enqueue Styles
-    wp_enqueue_style( 'custom-styles');
-    wp_enqueue_style( 'bootstrap-css' );
-
-
-    // Enqueue Scripts
-    wp_enqueue_script(  'main-js' );
-    wp_enqueue_script ( 'bootstrap-js' ); 
 }
 
 add_action( 'wp_enqueue_scripts', 'abso_enqueue_scripts' );
