9259903f5f63497d45b4ca0089ce530040ce2987
ChristosTsm <rayden.tsm@gmail.com>
2020-11-06 10:22:45 +0200

    custom logo/title

--- inc/classes/class-abso-theme.php
+++ inc/classes/class-abso-theme.php
@@ -15,13 +15,25 @@ class ABSO_THEME {
 
     protected function __construct() {
         //  Load other classes.
+        Assets::get_instance();
         $this->setup_hooks();
 
     }
 
     protected function setup_hooks() {
         //  Actions
-        Assets::get_instance();
+        add_action( 'after_setup_theme', [ $this, 'setup_theme' ] );
+    }
+
+    public function setup_theme() {
+        add_theme_support( 'title-tag' );
+        add_theme_support( 'custom-logo', [
+            'header-text' => [ 'site-title', 'site-description' ],
+            'height' => '100',
+            'width' => '300',
+            'flex-height' => true,
+            'flex-width' => true,
+        ]);
     }
 
 }
