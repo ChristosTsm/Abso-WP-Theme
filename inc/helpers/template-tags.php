9531ca8926033ca37edeab221ee6271eccdc0ba0
TsmRayden <rayden.tsm@gmail.com>
2020-11-10 11:02:45 +0200

    date meta tags

--- inc/helpers/template-tags.php
+++ inc/helpers/template-tags.php
@@ -37,3 +37,30 @@ function the_post_custom_thumbnail ( $post_id, $size = 'featured-thumbnail', $ad
     echo get_the_post_thumbnail( $post_id, $size, $additional_attributes );
 
 }
+
+function abso_posted_on() {
+    $time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
+
+
+    // Post is modified . (When post is publshed time is not equal to post modified time)
+    if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
+
+        $time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>'; 
+
+    }
+
+    $time_string = sprintf(
+        $time_string,
+        esc_attr( get_the_date( DATE_W3C ) ),
+        esc_attr( get_the_date( ) ),
+        esc_attr( get_the_modified_date( DATE_W3C ) ),
+        esc_attr( get_the_modified_date( ) ),
+    );
+
+    $posted_on = sprintf(
+        esc_html_x( 'Posted on %s', 'post date', 'abso' ),
+        '<a href="' . esc_url( get_permalink() ) . '" rel="bookmark" >' . $time_string . '</a>'
+    );
+
+    echo '<span class="posted-on text-secondary">' . $posted_on . '</span>';
+}
---
+++ template-parts/components/blog/entry-meta.php
@@ -0,0 +1,14 @@
+<?php 
+/**
+ * 
+ * Template for post meta tags
+ * 
+ * @package Abso
+ */
+?>
+
+ <div class="entry-meta mb-3">
+    
+    <?php abso_posted_on(); ?>
+ 
+ </div>
