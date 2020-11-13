2238d420be9718f3d2fd4df9ba04962bf1af667f
TsmRayden <rayden.tsm@gmail.com>
2020-11-13 21:50:13 +0200

    setup sass files/webpack

--- assets/build/js/main.js
+++ assets/build/js/main.js
@@ -1,4 +1,4 @@
-/******/ (() => { // webpackBootstrap
+/******/ (function() { // webpackBootstrap
 /******/ 	var __webpack_modules__ = ({
 
 /***/ "./src/js/clock/index.js":
@@ -7,49 +7,67 @@
   \*******************************/
 /*! unknown exports (runtime-defined) */
 /*! runtime requirements:  */
-/***/ (() => {
+/***/ (function() {
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
 
 // main.js
 
 /****Clock*****/
 (function ($) {
-  class Clock {
-    constructor() {
+  var Clock = /*#__PURE__*/function () {
+    function Clock() {
+      _classCallCheck(this, Clock);
+
       this.initializeClock();
     }
 
-    initializeClock() {
-      let t = setInterval(() => this.time(), 1000);
-    }
+    _createClass(Clock, [{
+      key: "initializeClock",
+      value: function initializeClock() {
+        var _this = this;
 
-    numPad(str) {
-      let cStr = str.toString();
-      if (cStr.length < 2) str = 0 + cStr;
-      return str;
-    }
-
-    time() {
-      let currDate = new Date();
-      let currSec = currDate.getSeconds();
-      let currMin = currDate.getMinutes();
-      let curr24Hr = currDate.getHours();
-      let ampm = curr24Hr >= 12 ? 'pm' : 'am';
-      let currHr = curr24Hr % 12;
-      currHr = currHr ? currHr : 12;
-      let stringTime = currHr + ':' + this.numPad(currMin) + ':' + this.numPad(currSec);
-      const timeEmojiEl = $('#time-emoji');
-
-      if (curr24Hr >= 5 && curr24Hr <= 17) {
-        timeEmojiEl.text('🌞');
-      } else {
-        timeEmojiEl.text('🌜');
+        var t = setInterval(function () {
+          return _this.time();
+        }, 1000);
       }
+    }, {
+      key: "numPad",
+      value: function numPad(str) {
+        var cStr = str.toString();
+        if (cStr.length < 2) str = 0 + cStr;
+        return str;
+      }
+    }, {
+      key: "time",
+      value: function time() {
+        var currDate = new Date();
+        var currSec = currDate.getSeconds();
+        var currMin = currDate.getMinutes();
+        var curr24Hr = currDate.getHours();
+        var ampm = curr24Hr >= 12 ? 'pm' : 'am';
+        var currHr = curr24Hr % 12;
+        currHr = currHr ? currHr : 12;
+        var stringTime = currHr + ':' + this.numPad(currMin) + ':' + this.numPad(currSec);
+        var timeEmojiEl = $('#time-emoji');
 
-      $('#time').text(stringTime);
-      $('#ampm').text(ampm);
-    }
+        if (curr24Hr >= 5 && curr24Hr <= 17) {
+          timeEmojiEl.text('🌞');
+        } else {
+          timeEmojiEl.text('🌜');
+        }
 
-  }
+        $('#time').text(stringTime);
+        $('#ampm').text(ampm);
+      }
+    }]);
+
+    return Clock;
+  }();
 
   new Clock();
 })(jQuery);
@@ -63,15 +81,18 @@
 /*! namespace exports */
 /*! exports [not provided] [no usage info] */
 /*! runtime requirements: __webpack_require__, __webpack_require__.n, __webpack_require__.r, __webpack_exports__, __webpack_require__.* */
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _clock__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./clock */ "./src/js/clock/index.js");
 /* harmony import */ var _clock__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_clock__WEBPACK_IMPORTED_MODULE_0__);
-/* harmony import */ var _img_watchtower_jpg__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../img/watchtower.jpg */ "./src/img/watchtower.jpg");
+/* harmony import */ var _sass_main_scss__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../sass/main.scss */ "./src/sass/main.scss");
+/* harmony import */ var _img_watchtower_jpg__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../img/watchtower.jpg */ "./src/img/watchtower.jpg");
 // Import Clock Widget  Script
-; // Import Images
+; // Styles
+
+ // Import Images
 
 
 
@@ -84,15 +105,28 @@ __webpack_require__.r(__webpack_exports__);
 /*! namespace exports */
 /*! export default [provided] [no usage info] [missing usage info prevents renaming] */
 /*! other exports [not provided] [no usage info] */
-/*! runtime requirements: __webpack_exports__, __webpack_require__.r, __webpack_require__.d, __webpack_require__.* */
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+/*! runtime requirements: __webpack_exports__, __webpack_require__.r, __webpack_require__.* */
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
 __webpack_require__.r(__webpack_exports__);
-/* harmony export */ __webpack_require__.d(__webpack_exports__, {
-/* harmony export */   "default": () => __WEBPACK_DEFAULT_EXPORT__
-/* harmony export */ });
-/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ("../../src/img/watchtower.jpg");
+/* harmony default export */ __webpack_exports__["default"] = ("../../src/img/watchtower.jpg");
+
+/***/ }),
+
+/***/ "./src/sass/main.scss":
+/*!****************************!*\
+  !*** ./src/sass/main.scss ***!
+  \****************************/
+/*! namespace exports */
+/*! exports [not provided] [no usage info] */
+/*! runtime requirements: __webpack_require__.r, __webpack_exports__, __webpack_require__.* */
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
 
 /***/ })
 
@@ -123,44 +157,44 @@ __webpack_require__.r(__webpack_exports__);
 /******/ 	
 /************************************************************************/
 /******/ 	/* webpack/runtime/compat get default export */
-/******/ 	(() => {
+/******/ 	!function() {
 /******/ 		// getDefaultExport function for compatibility with non-harmony modules
-/******/ 		__webpack_require__.n = (module) => {
+/******/ 		__webpack_require__.n = function(module) {
 /******/ 			var getter = module && module.__esModule ?
-/******/ 				() => module['default'] :
-/******/ 				() => module;
+/******/ 				function() { return module['default']; } :
+/******/ 				function() { return module; };
 /******/ 			__webpack_require__.d(getter, { a: getter });
 /******/ 			return getter;
 /******/ 		};
-/******/ 	})();
+/******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/define property getters */
-/******/ 	(() => {
+/******/ 	!function() {
 /******/ 		// define getter functions for harmony exports
-/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 		__webpack_require__.d = function(exports, definition) {
 /******/ 			for(var key in definition) {
 /******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
 /******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
 /******/ 				}
 /******/ 			}
 /******/ 		};
-/******/ 	})();
+/******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
-/******/ 	(() => {
-/******/ 		__webpack_require__.o = (obj, prop) => Object.prototype.hasOwnProperty.call(obj, prop)
-/******/ 	})();
+/******/ 	!function() {
+/******/ 		__webpack_require__.o = function(obj, prop) { return Object.prototype.hasOwnProperty.call(obj, prop); }
+/******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/make namespace object */
-/******/ 	(() => {
+/******/ 	!function() {
 /******/ 		// define __esModule on exports
-/******/ 		__webpack_require__.r = (exports) => {
+/******/ 		__webpack_require__.r = function(exports) {
 /******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
 /******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
 /******/ 			}
 /******/ 			Object.defineProperty(exports, '__esModule', { value: true });
 /******/ 		};
-/******/ 	})();
+/******/ 	}();
 /******/ 	
 /************************************************************************/
 /******/ 	// startup
--- assets/src/js/main.js
+++ assets/src/js/main.js
@@ -1,6 +1,8 @@
 // Import Clock Widget  Script
 import './clock';
 
+// Styles
+import '../sass/main.scss';
 
 // Import Images
 import '../img/watchtower.jpg';
